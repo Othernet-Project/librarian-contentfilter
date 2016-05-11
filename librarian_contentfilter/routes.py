b2b67af2b344026a3bccb0085562c84dd69d720a
Andrean Franc <andrean.franc@gmail.com>
2016-05-11 10:42:33 +0200

    Add a couple comments

--- librarian_contentfilter/routes.py
+++ librarian_contentfilter/routes.py
@@ -1,4 +1,4 @@
-from bottle import request, redirect
+from bottle import request
 from bottle_utils.ajax import roca_view
 from bottle_utils.html import urlunquote
 from bottle_utils.i18n import lazy_gettext as _, i18n_url
@@ -24,15 +24,18 @@ def regions_handler(form=None):
            'contentfilter/_languages',
            template_func=template)
 def languages_handler():
+    # TODO: when class based route handlers enter the game, convert this too
     # region must be set for both get and post requests
     form_cls = get_region_form(request.app.config)
     form = form_cls(request.params)
     if not form.is_valid():
+        # invalid region selected, go back to region selection form
         return regions_handler(form=form)
     # get list of languages for selected region
     region = form.processed_data['region']
     region_languages = get_languages_of(region, request.app.config)
     if request.method == 'GET':
+        # initial language form load, requests the list of available languages
         return dict(region=region,
                     region_languages=region_languages,
                     selected_languages=[],
@@ -44,14 +47,14 @@ def languages_handler():
     has_invalid = any(lang not in region_languages
                       for lang in selected_languages)
     if not selected_languages or has_invalid:
-        # some of the chosen values were invalid
+        # some of the chosen languages were invalid or no languages were chosen
         return dict(region=region,
                     region_languages=region_languages,
                     selected_languages=selected_languages,
                     # Translators, message displayed as help text for
                     # language selection
                     message=_("Please select languages from the list below."))
-    # languages are valid, store them in setup file
+    # languages are valid, store them in setup file and inform other about it
     exts.setup.append({'contentfilter.region': region,
                        'contentfilter.languages': selected_languages})
     set_fsal_whitelist(request.app.config)
