f87030ea4edd1aad26e4333fee3249c19835c6f7
Andrean Franc <andrean.franc@gmail.com>
2016-05-11 09:39:37 +0200

    Change data_source path config param name

--- librarian_contentfilter/hooks.py
+++ librarian_contentfilter/hooks.py
@@ -3,7 +3,7 @@ from .helpers import load_data_source, set_fsal_whitelist
 
 
 def initialize(supervisor):
-    source_path = supervisor.config['contentfilter.data_source']
+    source_path = supervisor.config['contentfilter.data_source_path']
     data_source = load_data_source(source_path)
     # do not register dashboard plugin if there is not data source
     if data_source:
